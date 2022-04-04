b8a51364afeb7d0e47138d288a4be26fe1cc5ca4
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-04 22:25:14 +0600

    react bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import { Routes, Route, Link } from "react-router-dom";
+import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+
+    </Routes>
   );
 }
 
