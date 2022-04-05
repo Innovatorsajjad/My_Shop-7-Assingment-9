32bcdc8677b0937e937206be52b7fabb0e1c876c
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-05 06:09:41 +0600

    navBar edited

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
   <div>
      <Header/>
       <Routes>
-      
        <Route path='/' element={<Home/>} />
 
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,17 +4,19 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className=" navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+  <div className="container-fluid container">
+    <a className="navbar-brand fw-bold" href="#">My Shop</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div className="navbar-nav">
-        <a className="nav-link active" aria-current="page" href="#">Home</a>
-        <a className="nav-link" href="#">Features</a>
-        <a className="nav-link" href="#">Pricing</a>
-        <a className="nav-link disabled">Disabled</a>
+      <div className="navbar-nav ms-auto">
+        <a className="nav-link" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">Reviews</a>
+        <a className="nav-link" href="#">Dashboard</a>
+        <a className="nav-link" href="#">Blogs</a>
+        <a className="nav-link" href="#">About</a>
+
       </div>
     </div>
   </div>
