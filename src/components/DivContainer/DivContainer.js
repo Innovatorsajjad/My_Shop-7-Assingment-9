6ff3f6969196721a00b8a9232ddf62d5a6334aab
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2022-04-05 06:45:38 +0600

    div container added

--- src/components/DivContainer/DivContainer.js
+++ src/components/DivContainer/DivContainer.js
@@ -1,8 +1,16 @@
  import React from 'react';
+ import "./DivContainer.css"
  
  const DivContainer = () => {
      return (
-         <div>
+         <div className='Main_container d-flex row align-items-center justify-content-center container mt-5'>
+             <div className="TextContainer col-md-6">
+                 <h1 className='fw-bold'>Chose Your <span className='TomatoColor fw-bold'>Laptop</span></h1>
+                 <h3>Buy The best Product For you...</h3>
+             </div>
+             <div className="ImgContainer col-md-6">
+                 <img className='img-fluid rounded' src="https://cdn.mos.cms.futurecdn.net/6t8Zh249QiFmVnkQdCCtHK.jpg" alt="" />
+             </div>
              
          </div>
      );
